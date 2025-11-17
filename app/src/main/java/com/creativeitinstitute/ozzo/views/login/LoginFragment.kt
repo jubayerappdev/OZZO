f6f4fdc4d06b59e18920d799931980291394a921
jubo445 <juboraj335.ja@gmail.com>
2025-11-17 17:33:26 +0600

    BaseFragment, Progress Dialog etc implementation

--- app/src/main/java/com/creativeitinstitute/ozzo/views/login/LoginFragment.kt
+++ app/src/main/java/com/creativeitinstitute/ozzo/views/login/LoginFragment.kt
@@ -1,30 +1,14 @@
 package com.creativeitinstitute.ozzo.views.login
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.creativeitinstitute.ozzo.R
+import com.creativeitinstitute.ozzo.base.BaseFragment
 import com.creativeitinstitute.ozzo.databinding.FragmentLoginBinding
 import com.creativeitinstitute.ozzo.isEmpty
 
-class LoginFragment : Fragment() {
-    lateinit var binding: FragmentLoginBinding
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
-        setListener()
-        return binding.root
-    }
-
-    private fun setListener() {
-
+    override fun setListener() {
         with(binding){
             btnLogin.setOnClickListener {
                 etEmail.isEmpty()
@@ -34,6 +18,9 @@ class LoginFragment : Fragment() {
                 }
             }
         }
+    }
+
+    override fun allObserver() {
 
     }
 
--- app/src/main/java/com/creativeitinstitute/ozzo/views/register/RegisterFragment.kt
+++ app/src/main/java/com/creativeitinstitute/ozzo/views/register/RegisterFragment.kt
@@ -8,29 +8,19 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.creativeitinstitute.ozzo.R
+import com.creativeitinstitute.ozzo.base.BaseFragment
 import com.creativeitinstitute.ozzo.core.DataState
 import com.creativeitinstitute.ozzo.data.models.UserRegistration
 import com.creativeitinstitute.ozzo.databinding.FragmentLoginBinding
 import com.creativeitinstitute.ozzo.databinding.FragmentRegisterBinding
 import com.creativeitinstitute.ozzo.isEmpty
 
-class RegisterFragment : Fragment() {
-    lateinit var binding: FragmentRegisterBinding
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>
+    (FragmentRegisterBinding::inflate) {
+
     private val viewModel: RegistrationViewModel by viewModels ()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentRegisterBinding.inflate(inflater, container, false)
-        setListener()
-        registrationResponse()
-        return binding.root
-    }
-
-
-
-    private fun setListener() {
+    override fun setListener() {
 
         with(binding){
             btnRegister.setOnClickListener {
@@ -56,24 +46,34 @@ class RegisterFragment : Fragment() {
         }
 
     }
+
+
+    //OOAD -> Object Oriented Analysis Design (Code Design)
+    override fun allObserver() {
+        registrationResponse()
+
+    }
+
     private fun registrationResponse() {
 
         viewModel.registrationResponse.observe(viewLifecycleOwner){
 
             when(it) {
                 is DataState.Error -> {
+                    loading.dismiss()
                     Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
                 }
                 is DataState.Loading -> {
+                    loading.show()
                     Toast.makeText(context, "Loading....", Toast.LENGTH_SHORT).show()
                 }
                 is DataState.Success -> {
+                    loading.dismiss()
                     Toast.makeText(context, "created User : ${it.data}", Toast.LENGTH_SHORT).show()
 
                 }
             }
         }
-
 
     }
 
